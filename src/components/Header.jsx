a101adb34610fd1222c0434c7b8bae71906a8151
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-04 22:28:11 +0300

    show/hide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import AddTaskForm from "./AddTaskForm";
+import { useState } from "react";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  //? 📢 React doesn't update states instantly, it updates them in bulk. States are updated with async logic to avoid over-rendering
+  const handleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <header className="header">
       <h1>TASK TRACKER</h1>
-      <button className="btn">SHOW ADD TASK BAR</button>
-      <AddTaskForm />
+      <button onClick={handleShow} className="btn">
+        SHOW ADD TASK BAR
+      </button>
+      {show && <AddTaskForm />}
     </header>
   );
 };
