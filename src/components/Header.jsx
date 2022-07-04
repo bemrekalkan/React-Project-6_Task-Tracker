81c02ef5933b39096c1a8a0f64e21a38171a9dfc
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-04 20:05:01 +0300

    structure done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,5 @@
-import React from 'react'
-import Button from './Button'
-
 const Header = () => {
-    return (
-        <div>
-            <h1>Task Tracker</h1>
-            <Button />
-        </div>
-    )
-}
+  return <header>Header</header>;
+};
 
-export default Header
+export default Header;
