8380fcbae534bb53a763aab78dee98773360848d
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-04 23:23:05 +0300

    local

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ import { useState, useEffect } from "react";
 // import data from "../helper/starterData";
 
 const Home = () => {
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")));
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
